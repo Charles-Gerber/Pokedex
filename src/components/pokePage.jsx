75e1579ecf465ade9e9379029080ece3095c192d
chgerber <chgerber@egencia.com>
2019-01-04 16:09:33 +0100

    pokemon selected display full

--- src/components/pokePage.jsx
+++ src/components/pokePage.jsx
@@ -11,9 +11,14 @@ class PokePage extends Component {
     if (this.props.pokemon) {
       return (
         <div className="card">
-          {this.props.pokemon.id} {this.props.pokemon.name}
           <img src={this.props.pokemon.image} alt="The selected pokemon" />
-          {this.printTypes()}
+          <h2>
+            #{this.props.pokemon.id} {this.props.pokemon.name}
+          </h2>
+          <h6>Types: {this.printTypes()}</h6>
+          <h6>Height: {this.props.pokemon.height}</h6>
+          <h6>Weight: {this.props.pokemon.weight}</h6>
+          <p>{this.props.pokemon.description}</p>
           <button
             className="btn btn-primary btn-sm"
             onClick={() => this.props.onLike(this.props.pokemon.id)}
